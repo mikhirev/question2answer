69de6fc9e5c777604eade2dc94d3911a5f024348
Dmitry Mikhirev <mikhirev@mezon.ru>
2013-09-01 11:04:38 +0400

    change doctype to XHTML 1.0 Transitional

--- qa-include/qa-theme-base.php
+++ qa-include/qa-theme-base.php
@@ -194,13 +194,14 @@
 
 		function doctype()
 		{
-			$this->output('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">');
+			$this->output('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+               "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">');
 		}
 		
 		function html()
 		{
 			$this->output(
-				'<html>',
+				'<html xmlns="http://www.w3.org/1999/xhtml">',
 				'<!-- Powered by Question2Answer - http://www.question2answer.org/ -->'
 			);
 			
